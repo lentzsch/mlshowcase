b1a5bbccfc93ac5226013903c6445a791f7536fe
James Lentzsch <jrlentzsch@gmail.com>
2022-10-18 11:19:58 -0700

    controlls and forward/reverse movement added

--- self_driving_car/car.js
+++ self_driving_car/car.js
@@ -4,6 +4,18 @@ class Car{
         this.y = y;
         this.width = width;
         this.height = height;
+
+        this.controls = new Controls();
+    }
+    
+    update () {
+        if(this.controls.forward){
+            this.y -= 2;
+        }
+        if(this.controls.revers){
+            this.y += 2;
+        }
+        
     }
 
     draw(ctx){
