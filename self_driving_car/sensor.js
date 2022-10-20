8bd27cd5d1ffa243714b94e0b3309d49691b7099
James Lentzsch <jrlentzsch@gmail.com>
2022-10-20 15:48:15 -0700

    sensor rays created and drawn

--- self_driving_car/sensor.js
+++ self_driving_car/sensor.js
@@ -1,29 +1,52 @@
 class Sensor {
     constructor(car) {
         this.car = car;
-        this.rayCount = 3;
-        this.rayLength = 100;
-        this.raySpread = Math.PI/4;
+        this.rayCount = 5;
+        this.rayLength = 150;
+        this.raySpread = Math.PI / 2;
 
         this.rays = [];
     }
 
     update() {
+        this.#castRays();
+    }
+
+    #castRays() {
         this.rays = [];
 
+        //determine ray angles from point of origin
         for (let i = 0; i < this.rayCount; i++) {
             const rayAngle = lerp(
-                this.raySpread/2,
-                -this.raySpread/2,
-                i/(this.rayCount - 1)
-            );
+                this.raySpread / 2,
+                -this.raySpread / 2,
+                this.rayCount == 1 ? 0.5 : i / (this.rayCount - 1)
+            ) + this.car.angle;
 
-            const start = {x: this.car.x, y: this.car.y};
+            //determine start and end point of rays as well as angles in relation to the car
+            const start = { x: this.car.x, y: this.car.y };
             const end = {
                 x: this.car.x - Math.sin(rayAngle) * this.rayLength,
                 y: this.car.y - Math.cos(rayAngle) * this.rayLength
             };
             this.rays.push([start, end]);
+        }
+    }
+
+    draw(ctx) {
+        for (let i = 0; i < this.rayCount; i++) {
+            ctx.beginPath();
+            ctx.lineWidth = 2;
+            ctx.strokeStyle = "yellow";
+            ctx.moveTo(
+                this.rays[i][0].x,
+                this.rays[i][0].y
+            );
+            ctx.lineTo(
+                this.rays[i][1].x,
+                this.rays[i][1].y
+            );
+            ctx.stroke();
         }
     }
 }
