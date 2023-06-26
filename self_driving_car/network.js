9b7e46262e08f2e4b64f3ba082429f61b640d640
James Lentzsch <jrlentzsch@gmail.com>
2023-06-26 11:58:57 -0700

    network.js created and script added to index.html

---
+++ self_driving_car/network.js
@@ -0,0 +1,22 @@
+class Level {
+    constructor (inputCount, outputCount) {
+        this.inputs = new Array(inputCount);
+        this.outputs = new Array(outputCount);
+        this.biases = new Array(outputCount);
+
+        this.weights = [];
+        for (let i = 0; i < inputCount; i++) {
+            this.weights[i] = new Array(outputCount)
+        }
+
+        level.#randomize(this);
+    }
+
+    static #randomize(level) {
+        for (let i = 0; i < level.imputs.length; i++) {
+            for (let j = 0; j < level.outputs.length; j++) {
+                level.weights[i][j] = Math.random() * 2 - 1;
+            }
+        }
+    }
+}
